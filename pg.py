cac0b22751a4e0522874045070dabf84270c59d8
Yi Chung Chen <yichung.chen.84@gmail.com>
2021-10-19 18:26:47 -0500

    boundary

--- pg.py
+++ pg.py
@@ -5,27 +5,43 @@ import os
 import sys
 
 import pygame
-
+import time
 ####
 pygame.init()
 
+####
 white=(255,255,255)
 black=(0,0,0)
 red=(255,0,0)
+dis_width = 800
+dis_height = 600
 
-dis=pygame.display.set_mode((800,600))
+snake_block = 10
+snake_speed = 30
+
+######
+
+dis=pygame.display.set_mode((dis_width,dis_height))
 # pygame.display.update()
 pygame.display.set_caption('snake')
 
-x1 = 300
-y1 = 300
+
+game_over = False
+
+x1 = dis_width/2
+y1 = dis_height/2
+
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
 
+font_style = pygame.font.SysFont(None,50)
 
-game_over = False
+def message(msg,color):
+    mesg = font_style.render(msg,True,color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
+
 while not game_over:
     for event in pygame.event.get():
         print(event)
@@ -33,24 +49,32 @@ while not game_over:
             game_over = True
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:
-                x1_change=-10
+                x1_change=-snake_block
                 y1_change=0
             if event.key==pygame.K_RIGHT:
-                x1_change=10
+                x1_change=snake_block
                 y1_change=0
             if event.key==pygame.K_UP:
                 x1_change=0
-                y1_change=-10
+                y1_change=-snake_block
             if event.key==pygame.K_DOWN:
                 x1_change=0
-                y1_change=10
+                y1_change=snake_block
+    
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
+    
     x1 += x1_change
     y1 += y1_change
     dis.fill(white)
-    pygame.draw.rect(dis,black,[x1,y1,10,10])
+    pygame.draw.rect(dis,black,[x1,y1,snake_block,snake_block])
     # pygame.draw.rect(dis,red,[100,100,10,10])
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(snake_speed)
+
+message("You Lost",red)
+pygame.display.update()
+time.sleep(2)
 
 pygame.quit()
 
